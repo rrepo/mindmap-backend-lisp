f24582037a56a3842b6e8f545a4abbf645ecb4fe
repo <reporepo254@gmail.com>
2025-09-22 21:21:09 +0900

    add env

--- utils/env.lisp
+++ utils/env.lisp
@@ -1,10 +1,13 @@
-(defpackage :env-utils
-  (:use :cl :cl-dotenv)
-  (:export))
+(defpackage :utils-env
+  (:use :cl)
+  (:export :*backend-token-secret* :load-env)) ;; ← export するシンボルを明示
 
-(in-package :env-utils)
+(in-package :utils-env)
 
-(.env:load-env (merge-pathnames ".env"))
+(defvar *backend-token-secret* nil)
 
-(defvar *backend-token-secret*
-        (uiop:getenv "BACKEND_TOKEN_SECRET"))
+;; 環境変数をロードする関数にしたほうが安全
+(defun load-env ()
+  (format *error-output* "Loading .env file~%")
+  (.env:load-env (merge-pathnames ".env"))
+  (setf *backend-token-secret* (uiop:getenv "BACKEND_TOKEN_SECRET")))
