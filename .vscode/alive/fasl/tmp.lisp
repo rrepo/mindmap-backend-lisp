c1fbf9e397e05b8781ed46d0763c59030daf58c7
repo <reporepo254@gmail.com>
2025-09-28 19:10:51 +0900

    add hotreload

--- .vscode/alive/fasl/tmp.lisp
+++ .vscode/alive/fasl/tmp.lisp
@@ -1,166 +1,58 @@
-(defpackage :websocket-app
-  (:use :cl :clack :websocket-driver :cl-dotenv)
-  (:export :start-app :*my-app* :stop-app))
+(in-package :cl-user)
 
-(in-package :websocket-app)
+(defparameter *dev-mode* t)
 
-(defmacro defroute-http (path &body body)
-  `(setf (gethash ,path *http-routes*)
-     (lambda (env) ,@body)))
+(defun dev-reloader (app)
+  (lambda (env)
+    (when *dev-mode*
+          (reload-dev))
+    (funcall app env)))
 
-(defmacro defroute-ws (path &body body)
-  `(setf (gethash ,path *ws-routes*)
-     (lambda (env)
-       (let ((ws (make-server env)))
-         ,@body
-         (lambda (responder)
-           (declare (ignore responder))
-           (start-connection ws))))))
-
-(defvar *http-routes* (make-hash-table :test #'equal))
-(defvar *ws-routes* (make-hash-table :test #'equal))
-
-(defvar *my-app*
-        ; (with-cors
-        (lambda (env)
-          (let* ((path (getf env :path-info))
-                 (method (getf env :request-method)))
-            (cond
-             ;; OPTIONSリクエスト (CORS preflight)
-             ((string= method "OPTIONS")
-               (list 200
-                     '(:content-type "text/plain")
-                     '("OK")))
-
-             ;; トークン認証（有効）
-             ((not (server-utils:validate-service-token env))
-               (list 401
-                     '(:content-type "text/plain")
-                     '("Unauthorized")))
-
-             ;; HTTPルート
-             ((gethash path *http-routes*)
-               (funcall (gethash path *http-routes*) env))
-
-             ;; WSルート
-             ((gethash path *ws-routes*)
-               (funcall (gethash path *ws-routes*) env))
-
-             ;; Not Found
-             (t
-               (list 404
-                     '(:content-type "text/plain")
-                     '("Not Found")))))))
-
-(defroute-http "/"
-               '(200 (:content-type "text/plain") ("Hello from /")))
-
-(defroute-http "/user"
-               (server-utils:with-api-response (controllers.users:handle-get-user env)))
-
-(defroute-http "/users"
-               (server-utils:with-api-response (controllers.users:handle-get-users env)))
-
-(defroute-http "/all-users"
-               (server-utils:with-api-response (controllers.users:handle-get-all-users)))
-
-(defroute-http "/create-user"
-               (server-utils:with-api-response (controllers.users:handle-create-user env)))
-
-(defroute-http "/update-user"
-               (server-utils:with-api-response (controllers.users:handle-update-user env)))
-
-(defroute-http "/delete-user"
-               (server-utils:with-api-response (controllers.users:handle-delete-user env)))
-
-(defroute-http "/get-map"
-               (server-utils:with-api-response (controllers.maps:handle-get-map env)))
-
-(defroute-http "/all-maps"
-               (server-utils:with-api-response (controllers.maps:handle-get-all-maps)))
-
-(defroute-http "/create-map"
-               (server-utils:with-api-response (controllers.maps:handle-create-map env)))
-
-(defroute-http "/update-map"
-               (server-utils:with-api-response (controllers.maps:handle-update-map env)))
-
-(defroute-http "/delete-map"
-               (server-utils:with-api-response (controllers.maps:handle-delete-map env)))
-
-(defroute-http "/all-nodes"
-               (server-utils:with-api-response (controllers.nodes:handle-get-all-nodes)))
-
-(defroute-http "/create-node"
-               (server-utils:with-api-response (controllers.nodes:handle-create-node env)))
-
-(defroute-http "/update-node"
-               (server-utils:with-api-response (controllers.nodes:handle-update-node env)))
-
-(defroute-http "/delete-node"
-               (server-utils:with-api-response (controllers.nodes:handle-delete-node env)))
-
-(defroute-http "/get-map-member"
-               (server-utils:with-api-response (controllers.map-members:handle-get-map-member env)))
-
-(defroute-http "/get-map-members-by-map-id"
-               (server-utils:with-api-response (controllers.map-members:handle-get-map-members-by-map-id env)))
-
-(defroute-http "/get-map-members-by-user-uid"
-               (server-utils:with-api-response (controllers.map-members:handle-get-map-members-by-user-uid env)))
-
-(defroute-http "/all-map-members"
-               (server-utils:with-api-response (controllers.map-members:handle-get-all-map-members)))
-
-(defroute-http "/create-map-member"
-               (server-utils:with-api-response (controllers.map-members:handle-create-map-member env)))
-
-(defroute-http "/delete-map-member"
-               (server-utils:with-api-response (controllers.map-members:handle-delete-map-member env)))
-
-(defroute-http "/get-map-invitation"
-               (server-utils:with-api-response (controllers.map-invitations:handle-get-map-invitation env)))
-
-(defroute-http "/get-map-invitation-by-token"
-               (server-utils:with-api-response (controllers.map-invitations:handle-get-map-invitation-by-token env)))
-
-(defroute-http "/get-map-invitation-by-map-id"
-               (server-utils:with-api-response (controllers.map-invitations:handle-get-map-invitation-by-map-id env)))
-
-(defroute-http "/create-map-invitation"
-               (server-utils:with-api-response (controllers.map-invitations:handle-create-map-invitation env)))
-
-(defroute-http "/delete-map-invitation"
-               (server-utils:with-api-response (controllers.map-invitations:handle-delete-map-invitation env)))
-
-(defroute-http "/get-map-details"
-               (server-utils:with-api-response (controllers.maps:handle-get-map-details env)))
-
-(defroute-ws "/websocket"
-             (on :message ws
-                 (lambda (msg)
-                   (format t "~&[WS] Received: ~A~%" msg)
-                   (send ws (concatenate 'string "Echo: " msg)))))
+(defun reload-dev ()
+  (dolist (file '("utils/utils"
+                  "models/initsql"
+                  "models/users"
+                  "models/maps"
+                  "models/nodes"
+                  "models/map-members"
+                  "models/map-invitations"
+                  "services/mindmaps"
+                  "controllers/users"
+                  "controllers/maps"
+                  "controllers/nodes"
+                  "controllers/map-members"
+                  "controllers/map-invitations"
+                  "utils/env"
+                  "utils/server-utils"
+                  "controllers/server"))
+    (format t "~%Reloading ~A...~%" file)
+    (handler-case
+        (load (asdf:system-relative-pathname "mindmap"
+                                             (format nil "~A.lisp" file)))
+      (error (e)
+        (format t "~%✗ Error while loading ~A: ~A~%" file e)
+        ;; エラーが出たらそれ以降はロードしない
+        (return)))))
 
 
-(defvar *current-server* nil
-        "現在起動中の Mindmap サーバーを保持。")
+(defun start-mindmap-server ()
+  (utils-env:load-env)
+  (init-db-utils:init-db)
+  (format t "Starting Mindmap server on port 5000...~%")
+  (setf *server*
+    (clack:clackup
+     (dev-reloader websocket-app::*my-app*)
+     :server :woo
+     :port 5000)))
 
-(defun start-app (&key (port 5000))
-  "サーバーを起動してサーバーオブジェクトを返す。"
-  ;; すでにサーバーがあれば停止してから起動
-  (when *current-server*
-        (stop-app))
-  (format t "Starting Mindmap server on port ~a...~%" port)
-  ;; Clack のサーバーを起動してオブジェクトを保存
-  (setf *current-server* (clack:clackup *my-app* :server :woo :port port))
-  *current-server*)
+(start-mindmap-server)
 
-(defun stop-app ()
-  "現在のサーバーを停止する。"
-  (when *current-server*
-        (format t "Stopping Mindmap server...~%")
-        (ignore-errors
-          ;; Clack/Woo 停止関数
-          (clack:stop *current-server*))
-        (setf *current-server* nil)))
+; (defun stop-mindmap-server ()
+;   "Mindmap サーバー停止"
+;   (websocket-app:stop-app))
+
+; (asdf:load-system :mindmap)
+; (start-mindmap-server)
+; (stop-mindmap-server)
+
+;  (uiop:run-program "clear" :output *standard-output*)
