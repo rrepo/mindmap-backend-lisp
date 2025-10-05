790616d559faa11f7b5fa0539782ab19222847ef
repo <reporepo254@gmail.com>
2025-10-05 18:26:25 +0900

    fix user

--- models/users.lisp
+++ models/users.lisp
@@ -22,12 +22,27 @@
    :rows :plists))
 
 (defun create-user (uid name &optional (img nil))
-  "Insert a new user into the users table. Returns :success or an error keyword."
-  (postmodern:execute
-   "INSERT INTO users (uid, name, img)
+  "Insert a new user into the users table.
+   If the UID already exists, do nothing and return :success."
+  (handler-case
+      (progn
+       (postmodern:execute
+        "INSERT INTO users (uid, name, img)
           VALUES ($1, $2, $3)"
-   uid name img)
-  :success :rows :plist)
+        uid name img)
+       :success)
+    (postmodern:database-error (e)
+                               ;; 重複キーエラーなら握りつぶして :success を返す
+                               (let ((msg (postmodern:database-error-message e)))
+                                 (if (search "duplicate key value violates unique constraint" msg)
+                                     (progn
+                                      (format *error-output* "Duplicate UID detected (~A), ignoring.~%" uid)
+                                      :success)
+                                     ;; 他のDBエラーはそのまま報告
+                                     (progn
+                                      (format *error-output* "Database error: ~A~%" msg)
+                                      :db-error))))))
+
 
 (defun update-user (uid &key name img)
   (when (or name img)
@@ -46,4 +61,3 @@
   (postmodern:execute
    "DELETE FROM users WHERE uid = $1"
    uid) :rows :plist)
-
