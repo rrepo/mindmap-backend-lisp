ede638179145ff961afe2d70bb2e038ab928ee95
repo <reporepo254@gmail.com>
2025-10-09 21:33:27 +0900

    fix

--- .vscode/alive/fasl/tmp.lisp
+++ .vscode/alive/fasl/tmp.lisp
@@ -1,68 +1,24 @@
-(defpackage :models.map-members
-  (:use :cl :postmodern)
-  (:export get-map-member
-           get-all-map-members
-           get-map-members-by-map-id
-           get-map-members-by-user-uid
-           create-map-member
-           delete-map-member))
+(defpackage :services.mindmaps
+  (:use :cl)
+  (:export get-map-details))
 
-(in-package :models.map-members)
+(in-package :services.mindmaps)
 
-(defun get-map-member (id)
-  "指定IDのmap_memberを取得する。"
-  (postmodern:query
-   "SELECT id, map_id, user_uid, created_at, updated_at
-    FROM map_members
-    WHERE id = $1"
-   id :rows :plist))
-
-(defun get-map-members-by-map-id (map-id)
-  "指定されたMAP_IDに属するメンバー一覧を取得する。"
-  (postmodern:query
-   "SELECT id, map_id, user_uid, created_at, updated_at
-    FROM map_members
-    WHERE map_id = $1
-    ORDER BY created_at ASC"
-   map-id
-   :rows :plists))
-
-(defun get-map-members-by-user-uid (user-uid)
-  "指定されたUSER_UIDに属するmap_membersの一覧を取得する。"
-  (postmodern:query
-   "SELECT id, map_id, user_uid, created_at, updated_at
-    FROM map_members
-    WHERE user_uid = $1
-    ORDER BY created_at ASC"
-   user-uid
-   :rows :plists))
-
-(defun get-all-map-members ()
-  "すべてのmap_memberを取得する。"
-  (postmodern:query
-   "SELECT id, map_id, user_uid, created_at, updated_at FROM map_members"
-   :rows :plists))
-
-(defun create-map-member (map-id user-uid)
-  "map_membersに新しいレコードを挿入する。すでに存在する場合は何もしない。"
-  (postmodern:execute
-   "INSERT INTO map_members (map_id, user_uid)
-    VALUES ($1, $2)
-    ON CONFLICT (map_id, user_uid) DO NOTHING"
-   map-id user-uid))
-
-; (defun update-map-member (id new-map-id new-user-uid)
-;   "map_membersの指定IDのmap_idとuser_uidを更新する。"
-;   (postmodern:execute
-;    "UPDATE map_members
-;     SET map_id = $1,
-;         user_uid = $2,
-;         updated_at = CURRENT_TIMESTAMP
-;     WHERE id = $3"
-;    new-map-id new-user-uid id))
-
-(defun delete-map-member (id)
-  "map_membersの指定IDのレコードを削除する。"
-  (postmodern:execute
-   "DELETE FROM map_members WHERE id = $1"
-   id))
+(defun get-map-details (map-uuid)
+  "指定 map-id の map と関連する情報を plist で返す"
+  (when map-uuid
+        (let* ((map (models.maps:get-map-by-uuid map-uuid))
+               (map-id (getf map :id))
+               (nodes (models.nodes:get-nodes-by-map-id map-id))
+               (members (models.map-members:get-map-members-by-map-id map-id))
+               (owner-uid (getf map :owner-uid))
+               (node-uids (mapcar (lambda (n) (getf n :user-uid)) nodes))
+               (member-uids (mapcar (lambda (m) (getf m :user-uid)) members))
+               (all-uids (remove-duplicates
+                             (append (list owner-uid) node-uids member-uids)
+                           :test #'string=))
+               (users (models.users:get-users all-uids)))
+          ;; --- まとめて返す ---
+          (append map
+            (list :nodes nodes
+                  :users users)))))
